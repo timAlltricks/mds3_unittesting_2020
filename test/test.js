93c7421e85bb6c7cfce4d89285597a8fbc1d033a
TimothéeLVB <50316572+timAlltricks@users.noreply.github.com>
2020-01-17 11:01:31 +0100

    finished

--- test/test.js
+++ test/test.js
@@ -43,21 +43,25 @@ describe('Checking Names', () => {
     const user = 'Jeff';
     //Check if the user is Jeff
     // Complete here
+    expect(functions.checkValue(user)).toBe('Jeff');
   });
 
   test('User is Karen', () => {
     const user = 'Karen';
     //Check if the user is Karen
     // Complete here
+    expect(functions.checkValue(user)).toBe('Karen');
   });
 });
 
 test('Adds 2 + 2 to equal 4', () => {
   // Complete here
+  expect(functions.add(2,2)).toBe(4);
 });
 
 test('Adds 2 + 2 to NOT equal 5', () => {
   // Complete here
+  expect(functions.add(2,2)).not.toBe(5);
 });
 
 /*
@@ -75,16 +79,23 @@ test('Adds 2 + 2 to NOT equal 5', () => {
 
 test('Should be null', () => {
   // Complete here
+  var test = null;
+  expect(functions.isNull(test)).toBeNull();
 });
 
 // toBeFalsy
 test('Should be falsy', () => {
   // Complete here
+  var test = false;
+  expect(test).toBeFalsy();
 });
 
 // toEqual
 test('User should be Marc Antoine object', () => {
   // Complete here
+  const user = functions.createUser();
+  expect(functions.checkValue(user.firstName)).toBe('Marc');
+  expect(functions.checkValue(user.lastName)).toBe('Antoine');
 });
 
 // Less than and greater than
@@ -92,25 +103,29 @@ test('Should be under or equal 1000', () => {
   const load1 = 500;
   const load2 = 500;
   // Complete here
+  expect(functions.add(load1, load2)).toBeLessThanOrEqual(1000);
 });
 
 // Regex
 test('There is no I in the word team', () => {
   // Complete here
+  expect('team').not.toMatch(/I/);
 });
 
 // Arrays
 test('Admin should be in usernames', () => {
   usernames = ['john', 'karen', 'admin'];
   // Complete here
+  expect(usernames).toContain('admin');
 });
 
 // Working with async data
 
 // Async Await since ES7
 test('User fetched name should be Leanne Graham', async () => {
-  const data = await functions.fetchUser();
   // Complete here
+  const data = await functions.fetchUser();
+  expect(functions.checkValue(data.name)).toBe('Leanne Graham');
 });
 
 /*
@@ -123,12 +138,15 @@ const reverseString = require('../src/reversestr');
 
 test('reverseString function exists', () => {
   // Complete here
+  expect(reverseString).toBeDefined();
 });
 
 test('String reverses', () => {
   // Complete here
+  expect(reverseString("tim")).toBe("mit");
 });
 
 test('String reverses with uppercase', () => {
   // Complete here
+  expect(reverseString("TIM")).toBe("mit");
 });
